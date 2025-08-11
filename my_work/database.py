86af0986d2777f566eab16ef9b46c1f2bdc807ed
Miracl1ee <bekinaek14@gmail.com>
2025-08-11 11:42:29 +0300

    Add files via upload

--- my_work/database.py
+++ my_work/database.py
@@ -1,46 +1,64 @@
-from sqlalchemy import create_engine, Index, Table, Column, Integer, String, MetaData, delete, text, insert
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from settings import db  
+from sqlalchemy import create_engine, Index, Column, Integer, String, text, insert
+from sqlalchemy.orm import declarative_base
+from settings import db
 
 Base = declarative_base()
+
 
 class Homework(Base):
     __tablename__ = 'homework'
     id = Column(Integer, primary_key=True)
     user_name = Column(String)
     house_work = Column(String)
-    __table_args__ = (Index('index', text("to_tsvector('russian', house_work)"), postgresql_using='gin'),)
+    __table_args__ = (Index
+                      ('id_homework_tsv',
+                       text("to_tsvector('russian', house_work)"),
+                       postgresql_using='gin'),)
+
 
 class Database():
     def __init__(self):
         self.engine = create_engine(db)
-        self.Session = sessionmaker(bind=self.engine)
+        self.homework = Homework.__table__
+        self.create_table()
 
     def create_table(self):
         Base.metadata.create_all(self.engine)
-            
+
     def add_homework(self, user_name, house_work):
+        with self.engine.begin() as connection:
+            add = insert(self.homework).values(
+                user_name=user_name,
+                house_work=house_work)
+            connection.execute(add)
+
+    def search_all(self, username):
         with self.engine.connect() as connection:
-            add = insert(Homework.__table__)
-            for work in house_work:
-                add = insert(self.homework).values({'user_name': user_name, 'house_work': work})
-                connection.execute(add)
-            connection.commit()
-    def delete_all(self):
-        with self.engine.connect() as connection:
-            d = delete(self.homework)
-            connection.execute(d)
-            connection.commit()
-            print('Все данные удалены')
-                
-    def search_homework(self, query):
-        find = text("""SELECT id, user_name, house_work FROM homework 
-                        WHERE to_tsvector('russian', house_work) @@ plainto_tsquery('russian', :query)""").bindparams(query=query)
-            
-        with self.engine.connect() as connection:
-            result = connection.execute(find) 
+            search = text("""
+                SELECT id, user_name, house_work
+                FROM homework
+                WHERE user_name = :username
+                """).bindparams(username=username)
+            result = connection.execute(search)
             return result.fetchall()
+
+    def search_with_user_and_text(self, username, query):
+        with self.engine.connect() as connection:
+            search = text("""
+                        SELECT id, user_name, house_work
+                        FROM homework
+                        WHERE user_name = :username
+                        AND to_tsvector('russian', house_work)
+                        @@ plainto_tsquery('russian', :query)
+                       """).bindparams(username=username, query=query)
+            result = connection.execute(search)
+            return result.fetchall()
+
+    def delete_all(self, username):
+        """Удаляет все задания конкретного пользователя"""
+        with self.engine.begin() as connection:
+            connection.execute(
+                text("DELETE FROM homework WHERE user_name = :username").bindparams(username = username))
 
     def close(self):
         self.engine.dispose()
--- my_work/file_format.py
+++ my_work/file_format.py
@@ -1,22 +1,25 @@
 import xlsxwriter
+
+
 class FileFormat():
-    def write_to_xlsx(self, lines):  
-        if not lines:
-            print('Нечего записывать в файл')
-            return 
-        workbook = xlsxwriter.Workbook('new_file.xlsx')
-        sheet1 = workbook.add_worksheet()
-        count=0
-        for line in lines:
-            sheet1.write(count, 0, line)
-            count+=1
-        workbook.close()
-        print('Ввод совершен')
-    def write_to_csv(self, lines):  
+    def write_to_xlsx(self, lines):
         if not lines:
             print('Нечего записывать в файл')
             return
-        with open('new_file.csv', 'w', newline='', encoding='utf-8') as file:
-            for line in lines:
-                file.write(line + '\n')
+        workbook = xlsxwriter.Workbook('new_file.xlsx')
+        sheet1 = workbook.add_worksheet()
+        count = 0
+        for line in lines:
+            sheet1.write(count, 0, line)
+            count += 1
+        workbook.close()
         print('Ввод совершен')
+        
+        def write_to_csv(self, line):
+            if not line:
+                print('Нечего записывать в файл')
+                return
+        with open('new_file.csv', 'a', newline='', encoding='utf-8') as file:
+            file.write(line + '\n')
+        print('Ввод совершен')
+        
