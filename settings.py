79111253ced3bbd6a5a4a9dc6a95143ea9be535f
Miracl1ee <bekinaek14@gmail.com>
2025-08-18 12:38:42 +0300

    добавлено

--- settings.py
+++ settings.py
@@ -3,5 +3,3 @@ import os
 load_dotenv()
 db = os.getenv("DATABASE_URL")
 TOKEN=os.getenv('token')
-API_URL = os.getenv('API_URL') or "http://192.168.0.152:8000"
-TIMEOUT = int(os.getenv('TIMEOUT', 30))
